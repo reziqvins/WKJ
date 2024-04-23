9b256c6a7459df7e441cef927677c2b8bf56fb0c
reziqvins <nreziq@gmail.com>
2024-04-23 07:29:32 +0700

    fix detailproduct

--- src/components/Store/ProductDetail/ProductInfo.jsx
+++ src/components/Store/ProductDetail/ProductInfo.jsx
@@ -30,7 +30,7 @@ const ProductInfo = ({ productInfo }) => {
       <hr />
       <p className="text-base text-gray-600">{productInfo.desc}</p>
 
-      <div className="flex items-center">
+      {/* <div className="flex items-center">
         <p className="text-sm mr-2"> leave a review </p>
 
         <svg
@@ -78,9 +78,9 @@ const ProductInfo = ({ productInfo }) => {
         >
           <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
         </svg>
-      </div>
+      </div> */}
 
-      <p className="text-base text-green-600 font-medium">En Stock</p>
+      <p className="text-base text-green-600 font-medium">Stok Tersedia</p>
       <p className="font-medium text-lg">
         <span className="font-normal">Kategori:</span> {productInfo.category}
       </p>
