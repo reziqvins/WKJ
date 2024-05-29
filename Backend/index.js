aa705f9226ec546810c06cd90a91dd1f60c2e987
reziqvins <nreziq@gmail.com>
2024-05-29 17:09:13 +0700

    be

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173', // Change this to your front-end URL
+    origin: '*', // Change this to your front-end URL
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization'
 }));
